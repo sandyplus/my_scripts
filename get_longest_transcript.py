1998daaad4ed9b303e187fab2b1c3f62a937fb96
sandy <twiliochina@gmail.com>
2017-12-23 20:55:15 +0800

    Update get_longest_transcript.py
    modifed script from internet

--- get_longest_transcript.py
+++ get_longest_transcript.py
@@ -1,34 +1,23 @@
 #!/usr/bin/env python
-longest_geneid = ''
-length_of_longest_geneid = 0
-transcript_id = ''
-lastline = ''
-fpa = open("out.gtf","w")
-with open("merged.gtf") as f:
-	for line in f:
-                # split string into substrings
-                subline = line.split("\t")
-                # first get the length of each transcript, the name of this gene
-                each_length = abs(int(subline[4]) - int(subline[3]))
-                each_geneid = subline[8][9:20]
-                each_transcript_id = subline[8][38:]
-                # judge if it is the same gene
-                if each_geneid != longest_geneid:
-                	print "length is %d" %length_of_longest_geneid
-                	print longest_geneid
-                	print transcript_id 
-			print >> fpa, lastline
-			lastline = line
-                	longest_geneid = each_geneid
-                	length_of_longest_geneid = each_length
-                	transcript_id = each_transcript_id
-                else:
-                	if each_length > length_of_longest_geneid:
-                        # second, get the longest transcript for each gene_id
-                        	length_of_longest_geneid = each_length
-                        	longest_geneid = each_geneid 
-                        	transcript_id = each_transcript_id
-				lastline = line
-                        #print "in else if, length is %d" %length_of_longest_geneid
-print >> fpa, lastline #print the last line of file
-fpa.close()
+from Bio import SeqIO
+import sys
+
+#please make sure that no space in the seq.id
+
+lastGene = None
+longest = (None, None)
+for rec in SeqIO.parse(sys.argv[1], "fasta"):
+    gene = rec.id.split("gene=")[-1]
+    l = len(rec)
+    if lastGene is not None:
+        if gene == lastGene:
+            if longest[0] < l:
+                longest = (l, rec)
+        else:
+            lastGene = gene
+            SeqIO.write(longest[1], sys.stdout, "fasta")
+            longest = (l, rec)
+    else:
+        lastGene = gene
+        longest = (l, rec)
+SeqIO.write(longest[1], sys.stdout, "fasta")
